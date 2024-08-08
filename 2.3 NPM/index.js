1f92dd831e91f958c4324d3557bf2fcf0ab2348f
YongYangyuen <47943307+YongYangyuen@users.noreply.github.com>
2024-08-08 15:38:19 +0700

    Add self learning node.js files.

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+// console.log("My name is " + sillyName + ".");
+
+import superheroNames from "superheroes"
+
+console.log(`I am ${superheroNames.random()}!`);
